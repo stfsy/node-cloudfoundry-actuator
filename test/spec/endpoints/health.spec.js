b368f3f830f854737d01cc5dc4c34b032ea1ad85
stfsy <s.pfaffel@gmail.com>
2017-10-03 09:40:44 +0200

    test: test health endpoint

---
+++ test/spec/endpoints/health.spec.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const resolve = require('path').resolve
+const expect = require('chai').expect
+const HealthEndpoint = require(resolve('lib/endpoints/health'))
+
+describe('HealthEndpoint', () => {
+    let endpoint = null
+    beforeEach(() => {
+        endpoint = new HealthEndpoint()
+    })
+    describe('.getPath', () => {
+        it('should return the endpoints name', () => {
+            expect(endpoint.getPath()).to.equal('/cloudfoundryapplication/health/')
+        })
+    })
+    describe('.getName', () => {
+        it('should return the endpoints name', () => {
+            expect(endpoint.getName()).to.equal('health')
+        })
+    })
+    describe('.handle', () => {
+        let mockResponse = () => {
+            const header = {}
+            return {
+                setHeader: (key, value) => header[key] = value,
+                getHeader: () => header
+            }
+        }
+        it('should set response headers', () => {
+            const response = mockResponse()
+            endpoint.handle({}, response)
+            expect(response.getHeader()['Access-Control-Allow-Origin']).to.equal('*')
+            expect(response.getHeader()['Access-Control-Allow-Headers']).to.equal('authorization, x-cf-app-instance')
+            expect(response.getHeader()['Access-Control-Allow-Methods']).to.equal('GET')
+        })
+        it('should return the current status', () => {
+            const response = mockResponse()
+            const result = endpoint.handle({}, response)
+            expect(result.status).to.equal('UP')
+        })
+    })
+})
