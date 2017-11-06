517314592a7215d4ad132e7a1b04b669b43960a6
stfsy <s.pfaffel@gmail.com>
2017-11-06 10:15:17 +0100

    chore: test instance aware endpoint

---
+++ test/spec/endpoints/instance-aware-endpoint.spec.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const resolve = require('path').resolve
+const expect = require('chai').expect
+const InstanceAwareEndpoint = require(resolve('lib/endpoints/instance-aware-endpoint'))
+
+describe('InstanceAwareEndpoint', () => {
+    let endpoint = null
+    beforeEach(() => {
+        endpoint = new InstanceAwareEndpoint({name: 'health'})
+    })
+    describe('.getPath', () => {
+        it('should return the endpoints name', () => {
+            expect(endpoint.getPath()).to.equal('/cloudfoundryapplication/health/')
+        })
+    })
+    describe('.getName', () => {
+        it('should return the endpoints name', () => {
+            expect(endpoint.getName()).to.equal('health')
+        })
+    })
+    describe('.handle', () => {
+        let mockResponse = () => {
+            const header = {}
+            return {
+                setHeader: (key, value) => header[key] = value,
+                getHeader: () => header
+            }
+        }
+        it('should set response headers', () => {
+            const response = mockResponse()
+            endpoint.handle({}, response)
+            expect(response.getHeader()['Access-Control-Allow-Origin']).to.equal('*')
+            expect(response.getHeader()['Access-Control-Allow-Headers']).to.equal('authorization, x-cf-app-instance')
+            expect(response.getHeader()['Access-Control-Allow-Methods']).to.equal('GET')
+        })
+    })
+})
