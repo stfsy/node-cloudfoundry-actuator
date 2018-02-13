36ebe3d6c14c6d995ba9a9f852c50568c71d57cd
stfsy <s.pfaffel@gmail.com>
2018-02-13 22:22:10 +0100

    test: test allow disabling of endpoints

--- test/spec/registry.spec.js
+++ test/spec/registry.spec.js
@@ -5,11 +5,11 @@ const expect = require('chai').expect
 const Registry = require(resolve('lib/registry'))
 
 describe('Registry', () => {
-    let registry = null
-    beforeEach(() => {
-        registry = new Registry()
-    })
     describe('.getEndpoint', () => {
+        let registry = null
+        beforeEach(() => {
+            registry = new Registry()
+        })
         it('should return info endpoint', () => {
             const endpoint = registry.getEndpoint({path: '/cloudfoundryapplication/info'})
             expect(endpoint.getName()).to.equal('info')
@@ -32,6 +32,35 @@ describe('Registry', () => {
         })
         it('should return undefined if the endpoint is unknown', () => {
             const endpoint = registry.getEndpoint({path: '/abc'})
+            expect(endpoint).to.be.undefined
+        })
+    })
+    describe('.constructor', () => {
+        it('allows disabling health endpoint', () => {
+            const registry = new Registry({
+                health: {
+                    enabled: false
+                }
+            })
+            const endpoint = registry.getEndpoint({path: '/cloudfoundryapplication/health/'})
+            expect(endpoint).to.be.undefined
+        })
+        it('allows disabling info endpoint', () => {
+            const registry = new Registry({
+                info: {
+                    enabled: false
+                }
+            })
+            const endpoint = registry.getEndpoint({path: '/cloudfoundryapplication/info/'})
+            expect(endpoint).to.be.undefined
+        })
+        it('allows disabling trace endpoint', () => {
+            const registry = new Registry({
+                trace: {
+                    enabled: false
+                }
+            })
+            const endpoint = registry.getEndpoint({path: '/cloudfoundryapplication/trace/'})
             expect(endpoint).to.be.undefined
         })
     })
