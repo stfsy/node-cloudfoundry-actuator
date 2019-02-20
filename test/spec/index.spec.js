6e2ed6b8e9944f5347b9642d284cc5b344afe988
stfsy <s.pfaffel@gmail.com>
2019-02-20 19:19:32 +0100

    test: test discovery endpoint

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -4,26 +4,39 @@ const resolve = require('path').resolve
 const expect = require('chai').expect
 const RequestHandler = require(resolve('lib/index'))
 
+const jwt = require('./security/cloudfoundry-uaa-mock-token-provider').createValidToken()
+
 describe('RequestHandler', () => {
     let handler = null
     beforeEach(() => {
         handler = new RequestHandler()
     })
-    it('.handle', () => {
+    it('._handle', () => {
         let sc = 0
-        const handled = handler.handle({
+        let body = ''
+        return handler._handle({
             path: '/cloudfoundryapplication/',
-            header: () => {}
+            header: () => 'bearer ' + jwt,
+            get: () => 'localhost:5122',
+            originalUrl: '/cloudfoundryapplication'
         }, {
             on: () => true,
             status: (statusCode) => {
                 sc = statusCode
                 return {
-                    send: () => {}
+                    send: (responseBody) => {
+                        body = responseBody
+                    }
                 }
+            },
+            setHeader: () => {
+
             }
+        }).then(() => {
+            expect(sc).to.equal(200)
+            expect(body).to.contain('/info')
+            expect(body).to.contain('/health')
+            expect(body).to.contain('/cloudfoundryapplication')
         })
-        expect(sc).to.equal(200)
-        expect(handled).to.equal(true)
     })
 })
